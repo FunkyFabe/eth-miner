b26073a41a1f80cd8ddbf7b2021337c007828290
Fabian Germann <fabian.germann@hsr.ch>
2021-07-22 10:52:01 +0200

    update paths

--- cron/cron.sh
+++ cron/cron.sh
@@ -3,8 +3,8 @@ util1=$(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits | a
 util2=$(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits | awk 'NR==2{print $1}')
 if [ $util1 -lt 80 ] || [ $util2 -lt 80 ]; then
   echo "restarting container"
-  cd /eth-miner
-  /usr/local/bin/docker-compose restart
+  cd /home/fabian/eth-miner
+  docker-compose restart
 else
   echo "continuing"
 fi
